389cce3b1cd4adbac3c4191943acecb275d71c57
dee <dee@adadevelopersacademy.org>
2019-04-08 16:34:23 -0700

    show test: existing and non-existant book

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -12,4 +12,34 @@ describe BooksController do
     must_respond_with :success
     #expect(response).must_be :success?
   end
+
+  describe "show" do
+
+    it "should be OK to show an existing, valid book" do
+
+      # Arrange
+      valid_book_id = 1
+
+      # Act
+      get "/books/#{valid_book_id}"
+
+      # Assert
+      must_respond_with :success
+
+    end
+
+    it "should give a 404 instead of showing a non-existant, invalid book" do
+
+      # Arrange
+      invalid_book_id = 999
+
+      # Act
+      get "/books/#{invalid_book_id}"
+
+      # Assert
+      must_respond_with :not_found
+
+    end
+
+  end
 end
