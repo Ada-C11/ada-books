c9ae7cad34c1cf9886178abb47fa3245eded0fc1
CheezItMan <chris@adadevelopersacademy.org>
2019-04-08 12:53:32 -0700

    added layout & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Method  path-URL, to: Controller#action
   get "/books", to: "books#index"
+  # get "/", to: "books#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
