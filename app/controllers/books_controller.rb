f211fc43335d1567741806571b267a0af03dc2e5
CheezItMan <chris@adadevelopersacademy.org>
2019-04-08 11:02:42 -0700

    Added index controller action for Books

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,11 @@
+class BooksController < ApplicationController
+  BOOKS = [
+    {title: "Hidden Figures", author: "Margot Lee Shetterly"},
+    {title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+    {title: "Kindred", author: "Octavia E. Butler"},
+  ]
+
+  def index
+    @books = BOOKS
+  end
+end
