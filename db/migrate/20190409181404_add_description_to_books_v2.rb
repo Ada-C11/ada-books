5a7ad669973d8b335516e41225cf06827bd20e30
dee <dee@adadevelopersacademy.org>
2019-04-10 11:23:52 -0700

    remove duplicate migration

--- db/migrate/20190409181404_add_description_to_books_v2.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToBooksV2 < ActiveRecord::Migration[5.2]
-  def change
-    add_column :books, :description, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_181404) do
+ActiveRecord::Schema.define(version: 2019_04_09_180418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,5 @@ ActiveRecord::Schema.define(version: 2019_04_09_181404) do
     t.datetime "updated_at", null: false
     t.string "description"
   end
+
 end
