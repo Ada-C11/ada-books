e3074330e0a5e895f2d197c9209015f753c4785e
dee <dee@adadevelopersacademy.org>
2019-04-12 11:41:12 -0700

    clean up routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,8 @@
 Rails.application.routes.draw do
-  # Method  path-URL, to: Controller#action
-  get "/books", to: "books#index", as: "books"
-
-  get "/books/new", to: "books#new", as: "new_book"
-  post "/books", to: "books#create"  #, as: "create_book"
-
-  get "/books/:id", to: "books#show", as: "book"
-
-  get "/books/:id/edit", to: "books#edit", as: "edit_book"
-  patch "/books/:id", to: "books#update"
-
-  delete "/books/:id", to: "books#destroy"
-
   root to: "books#index"
+
+  resources :books, except: [:destroy]
 
   patch "/books/:id/read", to: "books#mark_read", as: "mark_read"
 
-  # get "/", to: "books#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
