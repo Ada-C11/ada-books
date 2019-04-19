5dd370210f8e9b2926937d4b21382509c9ff9809
dee <dee@adadevelopersacademy.org>
2019-04-19 12:57:24 -0700

    update models book and genres to have and belong to each other

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ApplicationRecord
-    belongs_to :author   # singular
+  belongs_to :author   # singular
+  has_and_belongs_to_many :genres
 
-    validates :title, presence: true,   uniqueness: true
-
-    
+  validates :title, presence: true, uniqueness: true
 end
