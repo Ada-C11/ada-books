21a37da1900616871868645a18da0d28f7fedf9b
dee <dee@adadevelopersacademy.org>
2019-04-19 12:40:05 -0700

    creates a join table between books and genres for the many to many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_181757) do
+ActiveRecord::Schema.define(version: 2019_04_19_182927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2019_04_19_181757) do
     t.bigint "author_id"
     t.integer "publication_date"
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "books_genres", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "genre_id"
+    t.index ["book_id"], name: "index_books_genres_on_book_id"
+    t.index ["genre_id"], name: "index_books_genres_on_genre_id"
   end
 
   create_table "genres", force: :cascade do |t|
