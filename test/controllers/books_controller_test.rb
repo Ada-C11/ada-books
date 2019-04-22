3694ef6ebd7a34cfc7851d623bdea5f069661c64
CheezItMan <chris@adadevelopersacademy.org>
2019-04-22 12:59:44 -0700

    added fixture to books controller test

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -14,34 +14,32 @@ describe BooksController do
   end
 
   describe "show" do
-
     it "should be OK to show an existing, valid book" do
 
       # Arrange
-      book = Book.create(title: "test book", author_id: Author.create(name: "test").id)
-      valid_book_id = book.id
+      # book = Book.create(title: "test book", author_id: Author.create(name: "test").id)
+      valid_book_id = books(:oop_part_two).id
 
       # Act
       get book_path(valid_book_id)
 
       # Assert
       must_respond_with :success
-
     end
 
     it "should give a 404 instead of showing a non-existant, invalid book" do
 
       # Arrange
-      invalid_book_id = 999
+      book = books(:oop_part_two)
+      invalid_book_id = book.id
+      book.destroy
 
       # Act
       get book_path(invalid_book_id)
 
       # Assert
       must_respond_with :not_found
-
     end
-
   end
 
   describe "create" do
@@ -55,14 +53,14 @@ describe BooksController do
         "book": {
           title: input_title,
           author_id: Author.create(name: input_author).id,
-          description: input_description
-        }
+          description: input_description,
+        },
       }
 
       # Act
       expect {
         post books_path, params: test_input
-      }.must_change 'Book.count', 1
+      }.must_change "Book.count", 1
 
       # Assert
       new_book = Book.find_by(title: input_title)
@@ -72,7 +70,6 @@ describe BooksController do
       expect(new_book.description).must_equal input_description
 
       must_respond_with :redirect
-
     end
 
     it "will return a 400 with an invalid book" do
@@ -85,19 +82,18 @@ describe BooksController do
         "book": {
           title: input_title,
           author_id: Author.create(name: input_author).id,
-          description: input_description
-        }
+          description: input_description,
+        },
       }
 
       # Act
       expect {
         post books_path, params: test_input
-    }.wont_change "Book.count"
-    # .must_change "Book.count", 0
+      }.wont_change "Book.count"
+      # .must_change "Book.count", 0
 
       # Assert
       must_respond_with :bad_request
-
     end
   end
 
@@ -113,23 +109,22 @@ describe BooksController do
 
       book_to_update = Book.create(starter_input)
 
-      input_title = "99 Bottles of OOP" # Valid Title
+      input_title = "101 Bottles of OOP" # Valid Title
       input_author = "Sandi Metz"
       input_description = "A look at how to design object-oriented systems"
       test_input = {
         "book": {
           title: input_title,
           author_id: Author.create(name: input_author).id,
-          description: input_description
-        }
+          description: input_description,
+        },
       }
-
 
       # Act
       expect {
         patch book_path(book_to_update.id), params: test_input
-    }.wont_change "Book.count"
-    # .must_change "Book.count", 0
+      }.wont_change "Book.count"
+      # .must_change "Book.count", 0
 
       # Assert
       must_respond_with :redirect
@@ -141,39 +136,38 @@ describe BooksController do
 
     it "will return a bad_request (400) when asked to update with invalid data" do
 
-    # Arrange
-    starter_input = {
-      title: "Becoming",
-      author_id: Author.create(name: "Michelle Obama").id,
-      description: "A book by the 1st lady",
-    }
-
-    book_to_update = Book.create(starter_input)
-
-    input_title = "" # Invalid Title
-    input_author = "Sandi Metz"
-    input_description = "A look at how to design object-oriented systems"
-    test_input = {
-      "book": {
-        title: input_title,
-        author_id: Author.create(name: input_author).id,
-        description: input_description
+      # Arrange
+      starter_input = {
+        title: "Becoming",
+        author_id: Author.create(name: "Michelle Obama").id,
+        description: "A book by the 1st lady",
       }
-    }
 
+      book_to_update = Book.create(starter_input)
 
-    # Act
-    expect {
-      patch book_path(book_to_update.id), params: test_input
-  }.wont_change "Book.count"
-  # .must_change "Book.count", 0
+      input_title = "" # Invalid Title
+      input_author = "Sandi Metz"
+      input_description = "A look at how to design object-oriented systems"
+      test_input = {
+        "book": {
+          title: input_title,
+          author_id: Author.create(name: input_author).id,
+          description: input_description,
+        },
+      }
 
-    # Assert
-    must_respond_with :bad_request
-    book_to_update.reload
-    expect(book_to_update.title).must_equal starter_input[:title]
-    expect(book_to_update.author.name).must_equal Author.find(starter_input[:author_id]).name
-    expect(book_to_update.description).must_equal starter_input[:description]
+      # Act
+      expect {
+        patch book_path(book_to_update.id), params: test_input
+      }.wont_change "Book.count"
+      # .must_change "Book.count", 0
+
+      # Assert
+      must_respond_with :bad_request
+      book_to_update.reload
+      expect(book_to_update.title).must_equal starter_input[:title]
+      expect(book_to_update.author.name).must_equal Author.find(starter_input[:author_id]).name
+      expect(book_to_update.description).must_equal starter_input[:description]
     end
 
     # edge case: it should render a 404 if the book was not found
@@ -190,7 +184,6 @@ describe BooksController do
       # Should respond with not found
       # The count will change by 0, i.e. won't change
 
-
     end
 
     it "can delete a book" do
@@ -198,7 +191,7 @@ describe BooksController do
       new_book = Book.create(title: "The Martian", author_id: Author.create(name: "Someone").id)
 
       expect {
-        
+
         # Act
         delete book_path(new_book.id)
 
@@ -208,7 +201,5 @@ describe BooksController do
       must_respond_with :redirect
       must_redirect_to books_path
     end
-
-
   end
 end
