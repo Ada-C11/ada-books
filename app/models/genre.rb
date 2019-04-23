eb762ad2941936c5e783fecf4817d2e84c926920
CheezItMan <chris@adadevelopersacademy.org>
2019-04-23 10:27:31 -0700

    update genre model test

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,10 @@
 class Genre < ApplicationRecord
   has_and_belongs_to_many :books
+
+  validates :name, presence: true
+
+  def earliest_book
+    genre_books = self.books.order(:publication_date)
+    return genre_books.first.publication_date
+  end
 end
