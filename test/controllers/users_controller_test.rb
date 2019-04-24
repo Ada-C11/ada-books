623ff601411d344ae6988df464e52f53e2fef146
dee <dee@adadevelopersacademy.org>
2019-04-24 14:40:59 -0700

    first draft on current action in users controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "login" do
+    # I am going to reserve testing the different behaviors (between existing user or new user) for this set of tests, rather than in current
+  end
+
+  describe "current" do
+    it "responds with success if a user is logged in" do
+
+      # Arrange: We have to log in as a user by NOT manipulating session... we will do a login action!
+      logged_in_user = users(:dee)
+      post login_path, params: { user: { username: logged_in_user.username } }
+
+      # expect(session[:user_id]).must_equal logged_in_user.id
+
+      # Act: We need to still make a request to get to the users controller current action
+      get current_user_path
+
+      # Assert: Check that it responds with success
+      must_respond_with :success
+    end
+
+    it "responds with a redirect if no user is logged in" do
+      get current_user_path
+      must_respond_with :redirect
+    end
+  end
 end
