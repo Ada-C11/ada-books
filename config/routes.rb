cd21c9adb77cbcc58048aedc4c152c26917fcc4f
CheezItMan <chris@adadevelopersacademy.org>
2019-04-28 17:44:19 -0700

    omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,10 @@ Rails.application.routes.draw do
 
   # patch "/books/:id/read", to: "books#mark_read", as: "mark_read"
 
-  get "/login", to: "users#login_form", as: "login"
-  post "/login", to: "users#login"
-  post "/logout", to: "users#logout", as: "logout"
-  get "/users/current", to: "users#current", as: "current_user"
+  # get "/login", to: "users#login_form", as: "login"
+  # post "/login", to: "users#login"
+  # post "/logout", to: "users#logout", as: "logout"
+  # get "/users/current", to: "users#current", as: "current_user"
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "users#create"
 end
