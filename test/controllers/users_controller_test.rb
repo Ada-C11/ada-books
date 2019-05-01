7e7ab52e61ea78dc6c3bd88d1a11ea6b23d8cd42
CheezItMan <chris@adadevelopersacademy.org>
2019-05-01 10:08:12 -0700

    oauth testing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,10 +3,45 @@ require "test_helper"
 describe UsersController do
   describe "login" do
     # I am going to reserve testing the different behaviors (between existing user or new user) for this set of tests, rather than in current
+
+    it "can log in an existing user" do
+      # Arrange
+      user_count = User.count
+
+      # Act
+      user = perform_login
+
+      expect(user_count).must_equal User.count
+
+      # Should also test Flash notices
+
+      expect(session[:user_id]).must_equal user.id
+    end
+
+    it "can log in a new user" do
+      # Arrange
+      user = User.new(provider: "github", username: "bob", uid: 987, email: "bob@hope.com")
+
+      expect {
+        # Act
+        perform_login(user)
+        # Assert
+      }.must_change "User.count", 1
+
+      # Should also test Flash notices
+      user = User.find_by(uid: user.uid, provider: user.provider)
+
+      expect(session[:user_id]).must_equal user.id
+    end
+
+    it "will redirect back to root with a flash message if not coming from github" do
+      # Skip Auth hash creation
+    end
   end
 
   describe "current" do
     it "responds with success if a user is logged in" do
+      skip # will update when we do OAuth testing
 
       # Arrange: We have to log in as a user by NOT manipulating session... we will do a login action!
       logged_in_user = perform_login
